3ca506abbe84bf401da33fc460e2cc438c8e05cc
Alexandr Glushkovskiy <alexander@beckndevs.com>
2022-07-14 10:46:57 +0300

    added eslint and constant in load-users-service

--- src/app/services/load-users.service.ts
+++ src/app/services/load-users.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { ConstValues } from '../../environments/constants';
+
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +14,7 @@ export class LoadUsersService {
   ) { }
 
   getUsers(page, searchParam): Observable<any>{
-      if (searchParam.split('').length < 3){
+      if (searchParam.split('').length < ConstValues.MIN_SEARCH_CHARS){
         return this.http.get('https://swapi.dev/api/people/?page=' + page);
       } else {
         return this.http.get('https://swapi.dev/api/people/?search=' + searchParam);
